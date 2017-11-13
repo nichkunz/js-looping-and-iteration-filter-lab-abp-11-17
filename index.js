0e0580ec744d301ba22646c6427ec3c8d493a682
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-13 17:36:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file
+
+
+function findMatching(drivers, name) {
+  return drivers.filter(function(driver){
+    if (name.toLowerCase() === driver.toLowerCase()){
+      return true
+    }
+    else {
+      return false
+    }
+  })
+}
+
+function fuzzyMatch(drivers, fLetters) {
+  let lengthOfName = fLetters.length;
+  return drivers.filter(function(name){
+    return name.slice(0, lengthOfName) === fLetters;
+  })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function(driver){
+      return driver.name === name
+  })
+}
