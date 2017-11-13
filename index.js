7fc1c810ac8f7c2bab2b2b50320360172293a019
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-13 17:24:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function fuzzyMatch(drivers, fLetters) {
   let lengthOfName = fLetters.length;
   return drivers.filter(function(name){
     return name.slice(0, lengthOfName) === fLetters;
-  }
+  })
 }
